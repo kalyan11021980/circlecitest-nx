c08cbce55219b06a249faf89d5b64519465dba4d
Kalyan Chatterjee <kalyan11021980@gmail.com>
2023-03-18 16:59:33 -0400

    circleci update

--- apps/nesttest/src/app/app.controller.spec.ts
+++ apps/nesttest/src/app/app.controller.spec.ts
@@ -14,10 +14,10 @@ describe('AppController', () => {
   });
 
   describe('getData', () => {
-    it('should return "Welcome to nesttest!"', () => {
+    it('should return "Welcome to circlecitest!"', () => {
       const appController = app.get<AppController>(AppController);
       expect(appController.getData()).toEqual({
-        message: 'Welcome to nesttest!',
+        message: 'Welcome to circlecitest!',
       });
     });
   });
